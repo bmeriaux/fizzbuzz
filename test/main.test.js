c6a0642a7cc227ace22ed01116ae09cb1e763edf
Benoit Meriaux <bmeriaux@octo.com>
2017-05-16 19:07:38 +0200

    initial commit

---
+++ test/main.test.js
@@ -0,0 +1,9 @@
+"use strict";
+var expect = require("chai").expect;
+var main = require("../main");
+
+describe("fizzbuzz", () => {
+    it("should return ....", () => {
+        expect(main.fizzbuzz(2)).to.equal([]);
+    });
+});

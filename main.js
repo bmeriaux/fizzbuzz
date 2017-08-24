3a1c532b8f41588bca70117e9577c719ab091ce3
Benoit Meriaux <bmeriaux@octo.com>
2017-08-24 16:14:55 +0200

    end exercise

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 "use strict";
 
-exports.fizzbuzz = function(){
-
+exports.fizzbuzz = function (param) {
+    let arr = [];
+    for (let j = 1; j <= param; j++) {
+        if (j % 3 === 0) {
+            arr.push("fizz");
+        } else if (j % 5 === 0) {
+            arr.push("buzz");
+        } else {
+            arr.push(j);
+        }
+    }
+    return arr;
 };
--- test/main.test.js
+++ test/main.test.js
@@ -3,7 +3,13 @@ const expect = require("chai").expect;
 const main = require("../main");
 
 describe("fizzbuzz", () => {
-    it("should return ....", () => {
-        expect(main.fizzbuzz(2)).to.equal([]);
+    it("should return [1,2] when i=2", () => {
+        expect(main.fizzbuzz(2)).to.deep.equal([1, 2]);
+    });
+    it("should return [1,2,fizz] when i=3", () => {
+        expect(main.fizzbuzz(3)).to.deep.equal([1, 2, "fizz"]);
+    });
+    it("should return [1,2,fizz,4,buzz] when i=5", () => {
+        expect(main.fizzbuzz(5)).to.deep.equal([1, 2, "fizz", 4, "buzz"]);
     });
 });
